b4576d95858e698e2793d3d93990370fe2ab0bca
Pier107 <p.creanza@protonmail.com>
2024-10-03 20:41:04 +0200

    Add animations

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -6,20 +6,21 @@ interface TimelineEntryProps {
     title: string;
     description: string;
     badge?: string;
-    isDotted: boolean
+    isDotted: boolean;
+    index: number;
 }
 
-const TimelineEntry: React.FC<TimelineEntryProps> = ({ date, title, description, badge, isDotted }) => {
+const TimelineEntry: React.FC<TimelineEntryProps> = ({ date, title, description, badge, isDotted, index }) => {
     return (
             <li className="h-full py-1 table-row"> 
                 <div className="table-cell align-top">
-                    <time className="mr-5 text-right hidden md:block scroll-m-20 text-2xl font-semibold tracking-tight">{date}</time>
+                    <time className="mr-5 text-right hidden md:block scroll-m-20 text-2xl font-semibold tracking-tight" data-aos="zoom-in-left" data-aos-delay={index*100}>{date}</time>
                 </div>
                 <div className="flex justify-center p-0 min-h-1 h-full relative">
-                    <div className="h-9 w-9 rounded-full bg-black dark:bg-white"/>
+                    <div className="h-9 w-9 rounded-full bg-black dark:bg-white" data-aos={index != 0 && "zoom-in"}/>
                     <div className={`absolute h-full border-l-2 border-black borde dark:border-white ${isDotted && "border-dashed"}`} />
                 </div>
-                <div className="table-cell align-top max-w-2xl px-5 h-full">
+                <div className="table-cell align-top max-w-2xl px-5 h-full" data-aos="fade-up" data-aos-delay={index*100}>
                     <h5 className="md:hidden mt-1">{date}</h5>
                     <h3>{title}</h3>
                     <p className={"muted"}>{description}</p>
